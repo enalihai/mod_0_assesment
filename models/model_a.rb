412ec9c7a514b197b5e38af96483383953fd4943
Luke Pascale <lpluke5@gmail.com>
2021-10-22 16:14:56 -0600

    Add okc_airport model

---
+++ models/model_a.rb
@@ -0,0 +1,11 @@
+okc_airport = {
+  'name' => 'Will Rogers World Airport',
+  'size' => "12 square miles",
+  'number_of_airlines' => 6,
+  'number_of_terminals' => 2,
+  'has_foodcourt' => true,
+  'requires_masks' => true,
+  'airlines' => ['Delta', 'Southwest', 'United', 'Spirit', 'Frontier',
+    'American Airlines'],
+  'restaurants' => ['Taco Bell', 'McDonalds', 'Sbarros', 'Thai86']
+}
