6ca8e1d8be0325872cb325394da763f39eef0f79
Luke Pascale <lpluke5@gmail.com>
2021-10-22 16:16:58 -0600

    Add tacoma model

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+tacoma = {
+  'color' => 'Pearl',
+  'dealership' => 'Jim Norton Toyota of Oklahoma City',
+  'model_year' => 2020,
+  'price' => 40230.75,
+  'is_4wd' => true,
+  'has_heated_seats' => false,
+  'options' => ['GPS', 'VCS', 'ECRT'],
+  'similar_vehicles' => ['Chevy Colorado', 'Ford Ranger', 'Nissan Frontier']
+}
