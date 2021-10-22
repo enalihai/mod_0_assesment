dee97bb9f9c6f714a51c96476747bb50d0e7dd9f
Luke Pascale <lpluke5@gmail.com>
2021-10-22 16:12:16 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,19 @@
+famous_person="George Washington"
+
+is_active=false
+
+famous_person='Bruce Wayne'
+
+num_3=7
+num_9=4
+
+sum=num_9+num_3
+
+puts "The sum is #{sum}."
+
+animals = ['dogs', 'cats', 'fish']
+animals.shift(1)
+
+puts animals
+
+puts "famous_person is #{famous_person.length} characters long."
